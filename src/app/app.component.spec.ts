2020bd0be15f148c5d92dd1b188e3ac15dae03ef
Boris Wilhelms <mail@wille-zone.de>
2017-08-14 13:14:35 +0200

    Add documentation

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,14 @@
+import { by } from 'protractor';
 import { TestBed, async } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
 
+// 'describe' defines the test suite.
 describe('AppComponent Angular Test', () => {
+
+  // beforeEach will be execute before each spec.
   beforeEach(async(() => {
+    // creates and configures an Angular testing module
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
@@ -11,7 +16,10 @@ describe('AppComponent Angular Test', () => {
     }).compileComponents();
   }));
 
+  // 'it' defines a spec.
   it('should create the app', async(() => {
+    // 'createComponent' creates an instance the component to test
+    // after 'createComponent' TestBed is 'sealed' and can not be changed.
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
@@ -25,6 +33,7 @@ describe('AppComponent Angular Test', () => {
 
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
+    // 'detectChanges' triggers Angular change detection.
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
