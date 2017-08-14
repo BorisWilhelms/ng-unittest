cbd5a70e9857cdee92b854fd336e88c32620e66e
Boris Wilhelms <mail@wille-zone.de>
2017-08-14 14:20:43 +0200

    Shareing via variables

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,11 @@
 import { by } from 'protractor';
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
 
 describe('AppComponent Angular Test', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let app: AppComponent;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -13,30 +15,26 @@ describe('AppComponent Angular Test', () => {
     })
       .compileComponents()
       .then(() => {
-        // 'this' is shared across beforeEach, it and afterEach
-        // 'this' will be reseted after 'afterEach'
-        this.fixture = TestBed.createComponent(AppComponent);
-        this.app = this.fixture.debugElement.componentInstance;
+        fixture = TestBed.createComponent(AppComponent);
+        app = fixture.debugElement.componentInstance;
       });
   }));
 
   // Alternative to Promise.then
   // beforeEach(() => {
-  //   this.fixture = TestBed.createComponent(AppComponent);
-  //   this.app = this.fixture.debugElement.componentInstance;
+  //   fixture = TestBed.createComponent(AppComponent);
+  //   app = fixture.debugElement.componentInstance;
   // });
 
   it('should create the app', async(() => {
-    expect(this.app).toBeTruthy();
+    expect(app).toBeTruthy();
   }));
 
   it(`should have as title 'app'`, async(() => {
-    expect(this.app.title).toEqual('app');
+    expect(app.title).toEqual('app');
   }));
 
   it('should render title in a h1 tag', async(() => {
-    const fixture = this.fixture;
-    // 'detectChanges' triggers Angular change detection.
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
