118c0fe4184cdbe9007941dc18b94d594bc37065
Boris Wilhelms <mail@wille-zone.de>
2017-08-14 13:38:32 +0200

    This shareing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,36 +3,39 @@ import { TestBed, async } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
 
-// 'describe' defines the test suite.
 describe('AppComponent Angular Test', () => {
 
-  // beforeEach will be execute before each spec.
   beforeEach(async(() => {
-    // creates and configures an Angular testing module
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
-    }).compileComponents();
+    })
+      .compileComponents()
+      .then(() => {
+        // 'this' is shared across beforeEach, it and afterEach
+        // 'this' will be reseted after 'afterEach'
+        this.fixture = TestBed.createComponent(AppComponent);
+        this.app = this.fixture.debugElement.componentInstance;
+      });
   }));
 
-  // 'it' defines a spec.
+  // Alternative to Promise.then
+  // beforeEach(() => {
+  //   this.fixture = TestBed.createComponent(AppComponent);
+  //   this.app = this.fixture.debugElement.componentInstance;
+  // });
+
   it('should create the app', async(() => {
-    // 'createComponent' creates an instance the component to test
-    // after 'createComponent' TestBed is 'sealed' and can not be changed.
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+    expect(this.app).toBeTruthy();
   }));
 
   it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(this.app.title).toEqual('app');
   }));
 
   it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = this.fixture;
     // 'detectChanges' triggers Angular change detection.
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
